b088bd94d81db5092c32eb7200b531bc21d183b2
Daniel <danieladrianzen@gmail.com>
2025-09-17 18:47:40 -0400

    testing deployment

---
+++ consultia/frontend/src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiBase: 'https://medberos-tima-71ca28dd851e.herokuapp.com/api',
+  timaBase: 'https://medberos-tima-71ca28dd851e.herokuapp.com/tima',
+  wsBase:  'wss://medberos-tima-71ca28dd851e.herokuapp.com/ws'
+};
