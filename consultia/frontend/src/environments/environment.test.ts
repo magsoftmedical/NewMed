7bdfe0d57a9140f6d5c303de271e9a5e2613bec1
Daniel <danieladrianzen@gmail.com>
2025-09-18 01:36:32 -0400

    fixed wsBase in environment.test.ts

--- consultia/frontend/src/environments/environment.test.ts
+++ consultia/frontend/src/environments/environment.test.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiBase: 'https://medberos-tima-71ca28dd851e.herokuapp.com/api',
   timaBase: 'https://medberos-tima-71ca28dd851e.herokuapp.com/tima',
-  wsBase:  'wss://medberos-tima-71ca28dd851e.herokuapp.com/ws'
+  wsBase:  'wss://medberos-tima-71ca28dd851e.herokuapp.com'
 };
